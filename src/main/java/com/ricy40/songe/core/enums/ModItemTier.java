216698ed9f183ec26924805dd502eca8ad1f99e5
Ricy40 <60597171+Ricy40@users.noreply.github.com>
2021-10-04 21:46:27 +0100

    Added Songe Tool Tier

---
+++ src/main/java/com/ricy40/songe/core/enums/ModItemTier.java
@@ -0,0 +1,66 @@
+package com.ricy40.songe.core.enums;
+
+import com.ricy40.songe.core.init.ItemInit;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    SONGE(2, 220, 5.0f, 2.5f, 18,
+            () -> Ingredient.of(ItemInit.SONGE.get()))
+
+    ;
+
+
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    ModItemTier(int level, int uses, float speed, float damage, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
+        this.level = level;
+        this.uses = uses;
+        this.speed = speed;
+        this.damage = damage;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = new LazyValue<>(repairIngredient);
+    }
+
+
+    @Override
+    public int getUses() {
+        return uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return damage;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
+}
